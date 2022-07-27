ac36f9bb1bc6ce85b364c8e714379ee6e1c2b240
nr1017 <97573525+nr1017@users.noreply.github.com>
2022-07-27 17:49:20 +0900

    Add files via upload
    js추가

---
+++ main.js
@@ -0,0 +1,26 @@
+const target = document.querySelector("#dynamic");
+const string = "I want to be a Front-end Developer";
+const split = string.split("");
+
+
+function reset(){
+    target.textContent = "";
+    const resplit = string.split("");
+    dynamic(resplit);
+}
+
+function dynamic(arr){
+    if(arr.length > 0){
+        target.textContent += arr.shift();
+        setTimeout(function(){
+            dynamic(arr)}, 80)
+    } else {
+        setTimeout(reset, 3000);
+    }
+}
+dynamic(split);
+
+function blink(){
+    target.classList.toggle("active");
+}
+setInterval(blink, 500)

7abb6cc43fe30361eb6487ddeff6b31451d4e54c
nr1017 <97573525+nr1017@users.noreply.github.com>
2022-08-05 15:43:50 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -2,6 +2,9 @@ const target = document.querySelector("#dynamic");
 const string = "I want to be a Front-end Developer";
 const split = string.split("");
 
+const toggleBtn = document.querySelector('.header__toggleBtn');
+const nav = document.querySelector('.header__nav');
+const contact = document.querySelector('.header__contact');
 
 function reset(){
     target.textContent = "";
@@ -20,7 +23,15 @@ function dynamic(arr){
 }
 dynamic(split);
 
+
 function blink(){
     target.classList.toggle("active");
 }
 setInterval(blink, 500)
+
+function btnHandler(){
+    nav.classList.toggle('active');
+    contact.classList.toggle('active');    
+}
+
+toggleBtn.addEventListener('click', btnHandler);
